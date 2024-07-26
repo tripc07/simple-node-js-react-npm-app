d4917f5a6082c13684f07f40bd8bb8af71ed160a
tripc07 <claycoshatt20@gmail.com>
2024-07-26 00:38:49 -0400

    config
    utd

---
+++ swaggerConfig.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Node.js API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
